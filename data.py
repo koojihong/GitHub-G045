034122d32f3e15abd0e17e258d829954f0a75720
Koo Ji Hong <gamertonydoesgaming@gmail.com>
2026-05-09 14:10:44 +0800

    updated

--- data.py
+++ data.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,6 +12,24 @@ def register():
 @app.route("/home")
 def home_page():
     return render_template("dashboard.html")
+
+@app.route("/login.html")
+def login():
+    return render_template("login.html")
+
+@app.route("/terms.html")
+def terms():
+    return render_template("terms.html")
+
+@app.route("/privacy.html")
+def privacy():
+    return render_template("privacy.html")
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("login.html"))
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
